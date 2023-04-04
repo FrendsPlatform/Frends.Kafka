f8f701eed6efad10b12123bf10536b51f1414ddd
Riku Virtanen <virtanen.riku91@gmail.com>
2023-04-04 13:33:29 +0300

    Debugging docker image

--- Frends.Kafka.Produce/Frends.Kafka.Produce.Tests/Prebuild/prebuildCommand.ps1
+++ Frends.Kafka.Produce/Frends.Kafka.Produce.Tests/Prebuild/prebuildCommand.ps1
@@ -1,8 +1,10 @@
 ﻿cd ./Frends.Kafka.Produce.Tests
 
-$file = Join-Path -Path ($pwd).path -ChildPath "\Prebuild\docker-compose.yml"
-if (-not(Test-Path -Path $file -PathType Leaf)) {
+$file = Join-Path -Path ($pwd).path -ChildPath "docker-compose.yml"
+if (-not(Test-Path -Path $file)) {
 	curl -sSL https://raw.githubusercontent.com/bitnami/containers/main/bitnami/kafka/docker-compose.yml > docker-compose.yml
 }
 
 docker-compose up -d
+
+cd ../
